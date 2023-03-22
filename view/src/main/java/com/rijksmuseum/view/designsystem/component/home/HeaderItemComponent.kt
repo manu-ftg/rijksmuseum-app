a5df833f88a6823ba7f7409a62751a739201a0e1
Manuel Bejarano <manuel.bejarano@fillthegapp.com>
2023-03-22 20:42:37 +0100

    [ADD] Design adjustments

--- view/src/main/java/com/rijksmuseum/view/designsystem/component/home/HeaderItemComponent.kt
+++ view/src/main/java/com/rijksmuseum/view/designsystem/component/home/HeaderItemComponent.kt
@@ -2,12 +2,20 @@ package com.rijksmuseum.view.designsystem.component.home
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,10 +34,25 @@ fun HeaderItemComponent(
             SeparatorComponent()
         }
 
-        Text(
-            text = item.artist,
-            style = MaterialTheme.typography.h5,
-            modifier = Modifier.padding(24.dp))
+        Row(
+            modifier = Modifier
+                .padding(top = 16.dp, start = 16.dp, end = 16.dp, bottom = 8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                modifier = Modifier.size(32.dp),
+                imageVector = Icons.Filled.Person,
+                contentDescription = "Author icon")
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Text(
+                modifier = Modifier
+                    .weight(1f),
+                text = item.artist,
+                style = MaterialTheme.typography.h5
+            )
+        }
     }
 }
 
