e383b5a7984ad8f32e999de964bdd0195e3cb77e
Manuel Bejarano <83236438+manu-ftg@users.noreply.github.com>
2023-03-22 17:44:43 +0100

    Revert "Develop"

--- app/src/main/java/com/rijksmuseum/RijksmuseumApplication.kt
+++
@@ -1,9 +0,0 @@
-package com.rijksmuseum
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class RijksmuseumApplication: Application() {
-
-}
--- presentation/src/main/java/com/rijksmuseum/presentation/viewdata/ScreenState.kt
+++
@@ -1,9 +0,0 @@
-package com.rijksmuseum.presentation.viewdata
-
-sealed class ScreenState<out T> {
-    object Loading: ScreenState<Nothing>()
-
-    data class Error(val message: String? = null): ScreenState<Nothing>()
-
-    data class Loaded<out T>(val content: T): ScreenState<T>()
-}
